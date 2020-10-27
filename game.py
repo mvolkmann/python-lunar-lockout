16dadc593bd24462d5253857f4a0f4a6133e80eb
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-10-27 16:33:59 -0500

    big changes to search solution space

--- game.py
+++ game.py
@@ -1,21 +1,19 @@
-import math
-import random
-# import sys
-from typing import Dict, List, Optional, Tuple
+# global-statement: Using the global statement
+# pylint: disable=W0603
 
-# import matplotlib as mlp
-# from matplotlib.pylab import rand
-# import matplotlib.pyplot as plt
-# import numpy as np
+import math
+from typing import List, Tuple
 
 SIZE = 5
 CENTER = math.floor(SIZE / 2)
 TARGET = '#'
 
-Action = Tuple[str, str] # letter and direction
-Board = List[List[str]]
-Position = Tuple[int, int]
-Robots = Dict[str, Position]
+Action = Tuple[int, str]  # robot index and direction
+Position = Tuple[int, int]  # column and row indexes
+robot_ids = ('R', 'O', 'Y', 'G', 'B', 'P')
+robot_names = ('red', 'orange', 'yellow', 'green', 'blue', 'purple')
+# Order of robots in list is same as order of robot_ids.
+Robots = List[Position]
 
 # These are dx and dy values for directions.
 direction_deltas = {
@@ -33,143 +31,139 @@ direction_map = {
 directions = direction_map.keys()
 
 # List of robot data which includes the
-# letter, column, and row of each robot.
-# Column and row are zero-based.
-robots: Robots = {
-    # 'R': (4, 4),
-    # 'O': (4, 0),
-    # 'Y': (3, 3),
-    # 'G': (2, 1),
-    # 'B': (-1, -1),
-    # 'P': (1, 2)
-    'R': (4, 4),
-    'O': (3, 0),
-    'Y': (1, 4),
-    'G': (1, 1),
-    'B': (-1, -1),
-    'P': (3, 3)
-}
-letters = robots.keys()
+# zero-based column and row of each robot.
+robots: Robots = [
+    (4, 4),
+    (3, 0),
+    (1, 4),
+    (1, 1),
+    (-1, -1),
+    (3, 3)
+]
 
-def can_move(letter: str, direction: str) -> bool:
-    validate_letter(letter)
+solved = False
+
+def can_move(robot_index: int, direction: str, robots: Robots) -> bool:
     validate_direction(direction)
-    column, row = robots[letter]
-    dx, dy = direction_deltas[direction]
-    c = column
-    r = row
-    distance = 0
-    while True:
-        c += dx
-        r += dy
-        if not valid_cell(c, r):  # off the board
-            return False
-        cell = board[r][c]
-        if not cell in (' ', TARGET):  # hit another robot
-            break
-        distance += 1
-    return distance > 0
+    column, row = robots[robot_index]
 
-# def draw_board():
-#     plt.grid(True)
-#     ax = plt.gca()  # get current axes
+    for index, position in enumerate(robots):
+        if index == robot_index:
+            continue
+        c, r = position
 
-#     # Turn off x and y axis ticks and labels.
-#     ax.axes.xaxis.set_visible(False)
-#     ax.axes.yaxis.set_visible(False)
+        blocks: bool = \
+            (direction == 'U' and c == column and r < row - 1) or \
+            (direction == 'D' and c == column and r > row + 1) or \
+            (direction == 'L' and r == row and c < column - 1) or \
+            (direction == 'R' and r == row and c > column + 1)
+        if blocks:
+            return True
 
-#     r = rand(SIZE, SIZE)
-#     print('r =', r)
-#     for row in range(SIZE):
-#         for column in range(SIZE):
-#             ax.add_patch(mlp.patches.Rectangle((column, row), 1, 1))
-#     ax.imshow(r, interpolation='nearest')
-#     plt.show()
+    return False
 
-def get_board(robots: Robots) -> Board:
-    board = [[' '] * SIZE for _ in range(SIZE)]
-    board[CENTER][CENTER] = TARGET
+def get_cell(robots: Robots, column: int, row: int):
+    for index, position in enumerate(robots):
+        c, r = position
+        if c == column and r == row:
+            return robot_ids[index]
+    is_center = column == CENTER and row == CENTER
+    return '#' if is_center else ' '
 
-    for letter, position in robots.items():
-        column, row = position
-        if row >= 0 and column >= 0:
-            board[row][column] = letter
-    return board
-
-def get_distance() -> float:
-    """Get distance from red robot to target."""
-    column, row = robots['R']
-    return math.hypot(column - CENTER, row - CENTER)
-
-def get_possible_actions() -> List[Action]:
+def get_possible_actions(robots: Robots) -> List[Action]:
     """Get all possible actions."""
     actions = []
-    for letter in letters:
+    for robot_index in range(len(robots)):
         for direction in directions:
-            if can_move(letter, direction):
-                actions.append((letter, direction))
+            if can_move(robot_index, direction, robots):
+                actions.append((robot_index, direction))
     return actions
 
-def get_random_action() -> Optional[Action]:
-    actions = get_possible_actions()
-    if len(actions) == 0:
-        return None
+def print_action(label: str, action: Action) -> None:
+    index, direction = action
+    print(label, robot_names[index], direction_map[direction])
 
-    # Prefer to move R if possible.
-    r_actions = filter(lambda a: a[0] == 'R', actions)
-    first_r_action = next(r_actions, None)
-    if first_r_action:
-        return first_r_action
+def print_actions(actions: List[Action]) -> None:
+    print('Actions:')
+    for action in actions:
+        print_action('  ', action)
 
-    return random.choice(actions)
-
-def move_robot(letter: str, direction: str) -> None:
-    validate_letter(letter)
-    validate_direction(direction)
-    print('moving robot', letter, direction_map[direction])
-    column, row = robots[letter]
-    begin_distance = get_distance()
-
-    dx, dy = direction_deltas[direction]
-    c = column
-    r = row
-
-    while True:
-        c += dx
-        r += dy
-        if valid_cell(c, r):
-            cell = board[r][c]
-            if cell in (' ', TARGET):  # if not occupied
-                # Vacate current cell.
-                board[row][column] = ' '
-                # Move to new cell.
-                row = r
-                column = c
-                board[row][column] = letter
-                robots[letter] = (column, row)
-            else:
-                print_board(board)
-                if letter == 'R':
-                    end_distance = get_distance()
-                    # prefer moving red robot closer to target
-                    reward = begin_distance - end_distance
-                else:
-                    reward = -1  # prefer to move red robot
-                print('reward =', reward)
-                break
-        else:
-            raise Exception('invalid move')
-
-def print_board(board: Board) -> None:
+def print_board(robots: Robots) -> None:
     border = '+---' * SIZE + '+'
-    for row in board:
+    for row in range(SIZE):
         print(border)
-        print('| ' + ' | '.join(row) + ' |')
+        s = '|'
+        for column in range(SIZE):
+            s += ' ' + get_cell(robots, column, row) + ' |'
+        print(s)
     print(border)
 
-def solved() -> bool:
-    column, row = robots['R']
+def solve(robots: Robots, depth: int = 0) -> None:
+    global solved
+
+    print_board(robots)
+    if is_solved(robots):
+        solved = True  # prevents further actions
+        print('Solved!')
+        return
+
+    actions = get_possible_actions(robots)
+    if len(actions) == 0:
+        return
+
+    for action in actions:
+        if solved:
+            return
+        new_robots = take_action(action, robots)
+        solve(new_robots, depth + 1)  # recursive call
+
+def is_solved(robots: Robots) -> bool:
+    column, row = robots[0]  # red robot
     return column == CENTER and row == CENTER
+
+def take_action(action: Action, robots: Robots) -> Robots:
+    robot_index, direction = action
+    validate_direction(direction)
+
+    robot_name = robot_names[robot_index]
+    print('moving', robot_name, 'robot', direction_map[direction])
+    column, row = robots[robot_index]
+
+    # Find the robot that will block the move.
+    blocker = None
+    for index, position in enumerate(robots):
+        if index == robot_index:
+            continue
+        c, r = position
+
+        if direction == 'U' and c == column and r < row - 1:
+            if not blocker or c > blocker[0]:
+                blocker = position
+        elif direction == 'D' and c == column and r > row + 1:
+            if not blocker or c < blocker[0]:
+                blocker = position
+        elif direction == 'L' and r == row and c < column - 1:
+            if not blocker or r > blocker[0]:
+                blocker = position
+        elif direction == 'R' and r == row and c > column - 1:
+            if not blocker or r < blocker[0]:
+                blocker = position
+
+    if not blocker:
+        raise ValueError('invalid move')
+
+    new_robots = robots.copy()
+    c = blocker[0]
+    r = blocker[1]
+    if direction == 'U':
+        new_robots[robot_index] = (c, r + 1)
+    elif direction == 'D':
+        new_robots[robot_index] = (c, r - 1)
+    elif direction == 'L':
+        new_robots[robot_index] = (c + 1, r)
+    elif direction == 'R':
+        new_robots[robot_index] = (c - 1, r)
+    return new_robots
 
 def valid_cell(column: int, row: int) -> bool:
     return 0 <= column < SIZE and 0 <= row < SIZE
@@ -178,41 +172,4 @@ def validate_direction(direction: str) -> None:
     if not direction in directions:
         raise ValueError('invalid direction ' + direction)
 
-def validate_letter(letter: str) -> None:
-    if not letter in letters:
-        raise ValueError('invalid robot letter ' + letter)
-
-board = get_board(robots)
-
-# draw_board()
-# sys.exit()
-
-print_board(board)
-
-# print(can_move('R', 'U'))
-# print(can_move('R', 'D'))
-# print(can_move('R', 'L'))
-# print(can_move('R', 'R'))
-
-# move_robot('R', 'U')
-# move_robot('R', 'L')
-# move_robot('R', 'D')
-# move_robot('R', 'L')
-
-# Solution
-# move_robot('P', 'U')
-# move_robot('G', 'R')
-# move_robot('R', 'L')
-# move_robot('R', 'U')
-# if solved():
-#     print('You win!')
-
-while True:
-    action = get_random_action()
-    if action is None:
-        print('Failed to find solution.')
-        break
-    move_robot(*action)
-    if solved():
-        print('Solution found!')
-        break
+solve(robots)
